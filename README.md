fda8ca1576e17f375beb07335b8d0c11199525d2
Dimario Oliver <Dimario Oliver>
2023-03-13 09:38:30 -0400

    Adding remaining stub routes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # project-REST-rant
+
+REST-Rant is an app where users can review restaurants.
+
+
--- controllers/places.js
+++ controllers/places.js
@@ -5,3 +5,12 @@ router.get('/', (req, res) => {
 })
 
 module.exports = router
+
+
+/*const router = require('express').Router()
+
+router.get('/', (req, res) => {
+    res.send('GET /places')
+})
+
+module.exports = router*/
