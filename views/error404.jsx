04b88e2e8b03d3228e4e574abff86aaacae9b44a
Dimario Oliver <Dimario Oliver>
2023-03-22 21:58:27 -0400

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -5,6 +5,9 @@ function error404 () {
     return (
       <Def>
           <main>
+          <div><img src="/images/404-pic.jpg" alt="Error 404" /></div>
+          Photo by <a href="https://unsplash.com/@blakeverdoorn?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Blake Verdoorn</a> on <a href="https://unsplash.com/images/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
           </main>
@@ -13,3 +16,7 @@ function error404 () {
   }
   
 module.exports = error404
+
+
+  
+  
--- views/home.jsx
+++ views/home.jsx
@@ -3,16 +3,23 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-              <a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
+        <Def>
+<main>
+    <h1>HOME</h1>
+    <div>
+      <img src="/images/delicious-pizza.jpg" alt="Pizza" />
+      <div>
+      Photo by <a href="https://unsplash.com/ko/@briewilly?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Chad Montano</a> on <a href="https://unsplash.com/images/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        </div>
+    </div>
+    <a href="/places">
+        <button className="btn-primary">Places Page</button>
+    </a>
+</main>
 
-          </main>
-      </Def>
+        </Def>
     )
-  }
-  
+}
+
 module.exports = home
+
