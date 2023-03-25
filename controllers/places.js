1371a6393ced9786b5b9364a83f7fd9aebfa8749
Dimario Oliver <Dimario Oliver>
2023-03-25 14:17:52 -0400

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -47,8 +47,18 @@ router.put('/:id', (req, res) => {
     res.send('PUT /places/:id')
 })
 router.get('/:id/edit', (req, res) => {
-    res.send('GET /places/:id/edit')
-})
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place: places[id] })
+    }
+  })
+  
 router.delete('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
