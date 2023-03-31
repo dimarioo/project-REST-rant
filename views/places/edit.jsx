820a8288f7bae1e44be9019842db1b4b9fd6576d
Dimario Oliver <Dimario Oliver>
2023-03-31 18:57:31 -0400

    Connected Schema and Updated Routes

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -10,23 +10,23 @@ function edit_form (data) {
 
 
             <div className="row">
-                <div className='col-sm-6 to the form-group'>
+            <div className="col-sm-6 col-md-4 col-lg-3">
     <label htmlFor="name">Place Name</label>
     <input className="form-control" id="name" name="name" value={data.place.name} required />
   </div>
-  <div className="col-sm-6 to the form-group">
+  <div className="col-sm-6 col-md-4 col-lg-3">
     <label htmlFor="pic">Place Picture</label>
     <input className="form-control" id="pic" name="pic" value={data.place.pic} />
   </div>
-  <div className="col-sm-6 to the form-group">
+  <div className="col-sm-6 col-md-4 col-lg-3">
     <label htmlFor="city">City</label>
     <input className="form-control" id="city" name="city" value={data.place.city} />
   </div>
-  <div className="col-sm-6 to the form-group">
+  <div className="col-sm-6 col-md-4 col-lg-3">
     <label htmlFor="state">State</label>
     <input className="form-control" id="state" name="state"  value ={data.place.state} />
   </div>
-  <div className="col-sm-6 to the form-group">
+  <div className="col-sm-6 col-md-4 col-lg-3">
     <label htmlFor="cuisines">Cuisines</label>
     <input className="form-control" id="cuisines" name="cuisines" required value={data.place.cuisines} />
   </div>
@@ -40,4 +40,5 @@ function edit_form (data) {
 }
 
 module.exports = edit_form
+
 
