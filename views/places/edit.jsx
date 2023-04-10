bce96200187a721ac6340d3720b872a4676decc8
Dimario Oliver <Dimario Oliver>
2023-04-10 19:31:46 -0400

    Updated Delete and Edit Routes

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ function edit_form (data) {
         <Def>
           <main>
             <h1>Edit Place</h1>
-            <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+            <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
 
 
             <div className="row">
@@ -30,7 +30,16 @@ function edit_form (data) {
     <label htmlFor="cuisines">Cuisines</label>
     <input className="form-control" id="cuisines" name="cuisines" required value={data.place.cuisines} />
   </div>
-  <input className="btn btn-primary" type="submit" value="Add Place" />
+  <div className="form-group col-sm-4">
+            <label htmlFor="founded">Founded</label>
+            <input className="form-control" 
+              id="founded" 
+              name="founded" 
+              value={data.place.founded} 
+            />
+        </div>
+  <input className="btn btn-primary" type="submit" value="Update Place" />
+
   </div>
 </form>
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -17,16 +17,21 @@ function show (data) {
     let sumRatings = data.place.comments.reduce((tot, c) => {
       return tot+ c.stars
     }, 0)
-    let averageRating = sumRatings / data.place.comments.length
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+
+    for (let i = 0; i < averageRating; i++){
+      stars += "⭐️"
+    } 
     rating = (
       <h3>
-        {Math.round(averageRating)} stars
+        {stars} stars
       </h3>
     )
     comments = data.place.comments.map(c => {
       return (
         <div className="border">
-          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😄'}</h2>
           <h4>{c.content}</h4>
           <h3>
             <stong>- {c.author}</stong>
@@ -60,10 +65,10 @@ function show (data) {
             <h4>
             Serving {data.place.cuisines}
             </h4>     
-            <a id='edit' href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+            <a id='edit' href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
             Edit
                 </a> 
-                <form id='delete' method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <form id='delete' method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
                 <button type="submit" className="btn btn-danger">
                 Delete
                 </button>
