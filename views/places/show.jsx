34771e2317a4b909975c4c023393c4da608e1fb4
Dimario Oliver <Dimario Oliver>
2023-04-10 18:12:11 -0400

    Updated Rating System

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,7 +8,21 @@ function show (data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className='inactive'>
+      Not Yet Rated
+    </h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot+ c.stars
+    }, 0)
+    let averageRating = sumRatings / data.place.comments.length
+    rating = (
+      <h3>
+        {Math.round(averageRating)} stars
+      </h3>
+    )
     comments = data.place.comments.map(c => {
       return (
         <div className="border">
@@ -28,6 +42,8 @@ function show (data) {
             <div className='row'>
             <div className='col-sm-6'>
             <h1>{data.place.name}</h1>
+            <h2>Rating</h2>
+            {rating}
             <img id='showpagepic' src={data.place.pic} alt="restaurant pic" />
             <h3>
               Located in {data.place.city}, {data.place.state}
@@ -35,7 +51,6 @@ function show (data) {
             </div>
             <div className='col-sm-6'>  
             
-            <h1>Rating: Currently Unrated</h1>
             <h2>Comments</h2>
             {comments}
 
